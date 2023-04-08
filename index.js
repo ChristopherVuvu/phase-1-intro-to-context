d5ae402da0d06854182e60db97f43c6a1a7e7a50
ChristopherVuvu <vuvumumo93@gmail.com>
2023-04-08 23:37:56 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+// Set Employee checkin function
+//function to include date, month, year and time
+function checkIn(employeeId) {
+    const today = new Date();
+    const timestamp = today.toISOString().substring(0, 13) + '00'; // Format timestamp as YYYY-MM-DDTHH00
+    const record = `${employeeId},${timestamp},check-in\n`;
+    localStorage.setItem('timecards', localStorage.getItem('timecards', '') + record);
+}
+
+//Set employee checkout function
+//function to include date, month, year and time
+function checkOut(employeeId) {
+    const now = new Date();
+    const timestamp = now.toISOString().substring(0, 13) + '00'; // Format timestamp as YYYY-MM-DDTHH00
+    const record = `${employeeId},${timestamp},check-out\n`;
+    localStorage.setItem('timecards', localStorage.getItem('timecards', '') + record);
+}
